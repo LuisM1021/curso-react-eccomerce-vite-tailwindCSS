7b0c122c4144b1ca20a8e722e2fa2ee6ca7c8b9b
LuisM1021 <lmurallesp@miumg.edu.gt>
2024-03-23 09:53:52 -0600

    Clase 4, Enrutamiento hacia cada vista de la aplicación

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,4 +1,4 @@
-// import {useRoutes} from 'react-router-dom' //Import of the custom hook of react router dom
+import {useRoutes,BrowserRouter} from 'react-router-dom' //Import of the custom hook of react router dom
 import Home from '../Home'
 import MyAccount from '../MyAccount'
 import MyOrder from '../MyOrder'
@@ -8,17 +8,28 @@ import SignIn from '../SignIn'
 
 import './App.css'
 
+//En la función AppRoutes se guardan las rutas que se retornan 
+//de la función useRoutes
+const AppRoutes = ()=>{
+  //La variable routes guarda todas las rutas y coloca el componente 
+  //sobre cada ruta
+  let routes = useRoutes([
+    { path: '/',element: <Home />},
+    { path: '/my-account',element: <MyAccount />},
+    { path: '/my-order',element: <MyOrder />},
+    { path: '/my-orders',element: <MyOrders />},
+    { path: '/sign-in',element: <SignIn />},
+    { path: '/*',element: <NotFound />},
+  ])
+  return routes
+}
+
 function App() {
-  // let r
+  
     return (
-      <div className='bg-red-100'>
-        <Home/>
-        <MyAccount/>
-        <MyOrder/>
-        <MyOrders/>
-        <NotFound/>
-        <SignIn/>
-      </div>
+      <BrowserRouter> 
+        <AppRoutes/>
+      </BrowserRouter>
   )
 }
 
