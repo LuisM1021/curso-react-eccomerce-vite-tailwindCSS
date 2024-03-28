716390df89dc2e2e492bf76554fa1b6a0d944a1e
LuisM1021 <lmurallesp@miumg.edu.gt>
2024-03-28 09:34:26 -0600

    Clase 7, creación del componente card y diseño de la card

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,9 +1,11 @@
+import {Card} from "../../Components/Navbar/Card"
 import {Layout} from "../../Components/Navbar/Layout"
 
 function Home() {
     return (
       <Layout>
         HOME
+        <Card/>
       </Layout>
   )
 }
