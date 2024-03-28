8458acbffffc4ab4e97dabfad9cfbb428047d357
LuisM1021 <lmurallesp@miumg.edu.gt>
2024-03-28 10:55:17 -0600

    Clase 8, consumiendo la fakeStore API para obtener los datos y renderizar las cards con ellos, uso de useEffect y useState

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,13 +1,24 @@
 import {Card} from "../../Components/Navbar/Card"
 import {Layout} from "../../Components/Navbar/Layout"
-
+import {useState,useEffect} from 'react'
 function Home() {
+  const [items,setItems] = useState(null)
+
+  useEffect(()=>{
+    fetch('https://api.escuelajs.co/api/v1/products')
+    .then(res=>res.json())
+    .then(data=>setItems(data))
+  },[])
     return (
       <Layout>
         HOME
-        <Card/>
+        <div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'>
+          {
+            items?.map((item)=>(<Card key={item.id} data={item}/>))
+          }
+        </div>
       </Layout>
   )
 }
 
-export default Home
+export {Home}
