44d398ce354f4ecee98eaf9d4ab84fb962b18bad
LuisM1021 <lmurallesp@miumg.edu.gt>
2024-04-08 18:39:24 -0600

    Clase 23. Creación del componente Orders card para mostrar las órdenes desde la página MyOrders, aún falta renderizar las cards

--- src/Pages/MyOrder/index.jsx
+++ src/Pages/MyOrder/index.jsx
@@ -2,12 +2,19 @@ import { Layout } from "../../Components/Layout"
 import { useContext } from "react"
 import { ShoppingContext } from "../../Context"
 import { OrderCard } from "../../Components/OrderCard"
+import { Link } from "react-router-dom"
+import { ChevronLeftIcon } from "@heroicons/react/24/outline"
 
 function MyOrder() {
   const context = useContext(ShoppingContext)
     return (
       <Layout>
-        My order
+        <div className='flex items-center justify-center w-80 relative mb-6'>
+          <Link to='/my-orders' className='absolute left-0'>
+            <ChevronLeftIcon className='h-6 w-6 text-black cursor-pointer'/>
+          </Link>
+          <h1>My Orders</h1>
+        </div>
         <div>
           {
             context.order?.slice(-1)[0].products.map(product=>{
--- src/Pages/MyOrders/index.jsx
+++ src/Pages/MyOrders/index.jsx
@@ -1,9 +1,27 @@
 import { Layout } from "../../Components/Layout"
+import { OrdersCard } from "../../Components/OrdersCard"
+import { useContext } from "react"
+import { ShoppingContext } from "../../Context"
+import { Link } from "react-router-dom"
 
 function MyOrders() {
+  const context = useContext(ShoppingContext)
     return (
       <Layout>
-        My orders
+        <div className='flex items-center justify-center w-80 relative'>
+          <h1>My Orders</h1>
+        </div>
+        {
+          context.order?.map((order)=>{
+            (
+              <Link to={`/my-orders/${order.id}`}>
+                <OrdersCard 
+                totalPrice={order.totalPrice}
+                totalProducts={order.totalProducts}/>
+              </Link>
+            )
+          })
+        }
       </Layout>
   )
 }
