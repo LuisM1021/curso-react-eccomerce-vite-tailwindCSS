c7a336a2dad78f7e24d31c7e74fb692dcd1b6615
LuisM1021 <lmurallesp@miumg.edu.gt>
2024-04-12 14:50:20 -0600

    Clase 26. Se globalizó el estado items. Se agregó en el home el input para buscar por título y se guarda la búsqueda en el estado searchByTitle

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -1,15 +1,25 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 
 const ShoppingContext = createContext()
 
 function ShoppingContextProvider({children}){
+    // Get products
+    const [items,setItems] = useState(null)
+    useEffect(()=>{
+        fetch('https://api.escuelajs.co/api/v1/products')
+        .then(res=>res.json())
+        .then(data=>setItems(data))
+      },[])
+
     //Shopping cart: Increment quantity
     const [count,setCount] = useState(0)
 
     //Shopping cart: Add products to cart
     const [cartProducts,setCartProducts] = useState([])
+
     //Shopping cart: Order
     const [order,setOrder] = useState([])
+
     //Product Detail: open/close
     const [isProductDetailOpen,setIsProductDetailOpen] = useState(false)
     const openProductDetail = ()=>{
@@ -30,6 +40,9 @@ function ShoppingContextProvider({children}){
     //Product Detail: Show product
     const [productToShow,setProductToShow] = useState({images:['#'],title:''})
 
+    //Search products by title
+    const [searchByTitle,setSearchByTitle] = useState(null)
+    console.log(searchByTitle)
     return(
         <ShoppingContext.Provider value={{
             count,
@@ -45,7 +58,11 @@ function ShoppingContextProvider({children}){
             openCheckoutSideMenu,
             closeCheckoutSideMenu,
             order,
-            setOrder
+            setOrder,
+            items,
+            setItems,
+            searchByTitle, 
+            setSearchByTitle
         }}>
             {children}
         </ShoppingContext.Provider>
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,22 +1,23 @@
 import {Card} from "../../Components/Card"
 import {Layout} from "../../Components/Layout"
-import {useState,useEffect} from 'react'
 import { ProductDetail } from "../../Components/ProductDetail"
-
+import { ShoppingContext } from "../../Context"
+import { useContext } from "react"
 function Home() {
-  const [items,setItems] = useState(null)
-
-  useEffect(()=>{
-    fetch('https://api.escuelajs.co/api/v1/products')
-    .then(res=>res.json())
-    .then(data=>setItems(data))
-  },[])
+  const context = useContext(ShoppingContext)
     return (
       <Layout>
-        HOME
+        <div className='flex items-center justify-center w-80 relative mb-4'>
+          <h1 className='font-medium text-xl'>Exclusive Products</h1>
+        </div>
+        <input 
+        type='text' 
+        placeholder='Search a product' 
+        className='rounded-lg border border-black w-80 p-4 mb-4 focus:outline-none' 
+        onChange={(event)=>context.setSearchByTitle(event.target.value)}/>
         <div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'>
           {
-            items?.map((item)=>(<Card key={item.id} data={item}/>))
+            context.items?.map((item)=>(<Card key={item.id} data={item}/>))
           }
         </div>
         <ProductDetail/>
