4e8e3f613f4bc4272d8382429e74b3b9f39b05ba
LuisM1021 <lmurallesp@miumg.edu.gt>
2024-05-04 17:44:59 -0600

    Reajuste del navbar para mobile y tablet

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,6 +1,6 @@
 import {NavLink} from 'react-router-dom'
 import {ShoppingContext} from '../../Context'
-import { useContext,useState } from 'react'
+import { useContext } from 'react'
 import { ShoppingCartIcon } from '@heroicons/react/24/solid'
 
 function Navbar(){
@@ -11,11 +11,16 @@ function Navbar(){
         context.setResponsiveDisplayNavbar(false)
     }
     return(
-        <nav className={`flex lg:flex-row lg:justify-between lg:items-center lg:top-0 lg:fixed z-10 lg:w-full lg:h-20 lg:py-5 lg:px-8 text-sm font-light bg-white
-        max-lg:flex-col max-lg:left-0 max-lg:top-0 max-lg:h-screen max-lg:z-10 max-lg:justify-between max-lg:w-48
+        <nav className={`flex max-sm:w-48
+        max-lg:border max-lg:border-r-2 max-sm:shadow-lg max-lg:shadow-black
+        max-lg:flex-col max-lg:left-0 max-lg:top-0 max-lg:h-screen max-lg:z-10 max-lg:justify-between max-lg:w-60
+        lg:flex-row lg:justify-between lg:items-center lg:top-0 lg:fixed z-10 lg:w-full lg:h-20 lg:py-5 lg:px-8 text-sm font-light bg-white
         ${context.responsiveDisplayNavbar ? 'max-lg:fixed' : 'max-lg:hidden'}`}>
-            <ul className='flex lg:flex-row max-lg:flex-col items-center gap-3'>
-                <li className='font-semibold text-lg'>
+            <ul className='max-sm:gap-5 max-sm:text-lg
+            max-lg:text-xl max-lg:gap-6
+            flex lg:flex-row max-lg:flex-col items-center gap-3'>
+                <li className='max-lg:text-2xl max-lg:my-4
+                font-semibold text-lg'>
                     <NavLink
                     to={context.signIn ? '/':'/sign-in'}
                     onClick={()=>handleResponsiveDisplay()}
@@ -78,7 +83,9 @@ function Navbar(){
                     </NavLink>
                 </li>
             </ul>
-            <ul className='flex lg:flex-row max-lg:flex-col items-center gap-3'>
+            <ul className='max-sm:gap-5
+            max-lg:gap-6 max-lg:text-lg
+            flex lg:flex-row max-lg:flex-col items-center gap-3'>
                 {context.signIn && 
                     <li className='text-black/60'>
                         {context.currentAccount[0].email}
@@ -123,7 +130,7 @@ function Navbar(){
                                 Sign Up
                             </NavLink>
                 }</li>
-                <li className='flex items-center'>
+                <li className='max-lg:my-4 flex items-center'>
                     <ShoppingCartIcon className='h-6 w-6'/>
                     <div>{context.cartProducts.length}</div>
                 </li>
